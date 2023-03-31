5405151f8cb20f84f2ca09f476d4928022015450
Tori Lew <torirlew@gmail.com>
2023-03-30 20:23:51 -0700

    Closure concepts

--- Chapter02/ContentView.swift
+++ Chapter02/ContentView.swift
@@ -10,12 +10,18 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = true
     var body: some View {
-        VStack {
+        setup()
+    }
+    func setup()-> some View{
+        let number1 = 23
+        let number2 = 78
+        let sum = addTwoNumbers(number1, number2)
+       return VStack {
             Toggle(isOn: $message) {
                 Text("Toggle message on/off")
             }
             if message {
-                Text ("Here's a secret message!")
+                Text ("Hello, did you know that \(number1) + \(number2) = \(sum)?")
                     .background(Color.yellow)
                     .padding()
             }
@@ -27,4 +33,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+let addTwoNumbers = {( num1: Int, num2: Int) -> Int in
+    let sum = num1 + num2
+    print("\(num1) + \(num2) = \(sum)")
+    return sum
+    //return num1 + num2
 }
