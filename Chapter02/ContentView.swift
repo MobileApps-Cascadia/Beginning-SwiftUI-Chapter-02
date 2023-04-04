912e4a5591c6a5f30e115bf2a830abd2e453a588
Nick Eddington <neddington@cascadia.edu>
2023-04-04 16:38:03 -0700

    Requested Changes Made

--- Chapter02/ContentView.swift
+++ Chapter02/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = true
     @State private var buttonTapped = false
-    
+    @State private var speed = 50.0
+    @State private var isEditing = false
+    @State private var fullText: String = "This is some editable text..."
+
     var body: some View {
         setup()
             .padding()
@@ -22,6 +25,20 @@ struct ContentView: View {
         let total = age * 2
         
         return VStack {
+            
+            Slider(
+                value: $speed,
+                in: 0...100,
+                onEditingChanged: { editing in
+                    isEditing = editing
+                }
+            )
+            Text("\(speed)")
+                .foregroundColor(isEditing ? .red : .blue)
+            
+            Image(systemName: "house.circle.fill")
+            Image(systemName: "gear.circle")
+            
             Toggle(isOn: $message) {
                 Text("Toggle message on/off")
             }
@@ -39,7 +56,9 @@ struct ContentView: View {
                     .padding()
                     .background(Color.blue)
             }
+            TextEditor(text: $fullText)
         }
+        
     }
 }
 
